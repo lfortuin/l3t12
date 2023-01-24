853e5ae951c429d9a23741eed8c5153bbcf47ca4
lfortuin <121763797+lfortuin@users.noreply.github.com>
2023-01-24 21:18:26 +0200

    Add files via upload

--- semantic.py
+++ semantic.py
@@ -9,4 +9,15 @@ for token1 in tokens:
 
 #Note:  monkey and cat have a high similarity because they are both animals
 #       apple and banana have a high similarity becuase they are both fruits
-#       monkey and banana havea  high simiarity because monkeys eat bananas
+#       monkey and banana have a high simiarity because monkeys eat bananas
+
+"""When using 'en_core_web_sm' instead of 'en_core_web_md' the follwing warning is issued: 
+
+UserWarning: [W007] The model you're using has no word vectors loaded, so the result of 
+the Doc.similarity method will be based on the tagger, parser and NER, which may not
+give useful similarity judgements. This may happen if you're using one of the small 
+models, e.g. `en_core_web_sm`, which don't ship with word vectors and only use 
+context-sensitive tensors. You can always add your own word vectors, or use one of 
+the larger models instead if available.
+
+The similarities are lower when using 'en_core_web_sm' instead of 'en_core_web_md'"""
